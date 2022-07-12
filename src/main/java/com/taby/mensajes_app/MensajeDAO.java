ec451fab17ee925301ac20edd2ffd9623a3e8f37
Henry Taby <henry.taby@gmail.com>
2022-07-11 23:48:41 -0400

    CRUD: inserción de datos

--- src/main/java/com/taby/mensajes_app/MensajeDAO.java
+++ src/main/java/com/taby/mensajes_app/MensajeDAO.java
@@ -4,13 +4,33 @@
  */
 package com.taby.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author henry.taby
  */
 public class MensajeDAO {
     public static void crearMensajeDB(Mensajes mensaje){
-        
+        Conexion db_connect = new Conexion();
+        try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try{
+                String query="INSERT INTO mensajes (mensaje,autor_mensaje) VALUES (?,?)";
+                ps=conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2,mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("Mensaje creado");
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+            
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
     public static void leerMensajesDB(){
         
--- src/main/java/com/taby/mensajes_app/mensajesService.java
+++ src/main/java/com/taby/mensajes_app/mensajesService.java
@@ -3,14 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.taby.mensajes_app;
-
+import java.util.Scanner;
 /**
  *
  * @author henry.taby
  */
 public class mensajesService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+        System.out.println("Tu nombre:");
+        String nombre = sc.nextLine();
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
         
+        MensajeDAO.crearMensajeDB(registro);
     }
     public static void listarMensaje(){
         
